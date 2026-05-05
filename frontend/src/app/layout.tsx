0795af8b7b3b563a60be49eb8f79464d435b48c6
sandeep markondiah jayaprakash <sandeep.mjay@gmail.com>
2026-05-04 20:11:30 -0700

    fix(ui): suppress body hydration warnings from browser extensions
    Grammarly injects data-gr-* attributes on <body>, diverging from SSR HTML.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
         <script dangerouslySetInnerHTML={{ __html: themeBootstrap }} />
         <AppShell>{children}</AppShell>
       </body>
